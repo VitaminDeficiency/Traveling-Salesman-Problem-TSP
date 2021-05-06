e3d5e521b39fd188526a14508d8ac86c13797ea7
lackvitamin <nguyenhoaiy7@gmail.com>
2021-05-06 16:03:57 +0700

    Update All File

--- _heapSort.h
+++ _heapSort.h
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 void swap(float *a, float *b)
 {
     float temp;
--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "_heapSort.h"
 
-int main(){
+int main()
+{
     int i;
-    float a[8] = {3,4,1,2,7,5,9};
-    _heapSort(a,6);
-    for(i = 0; i < 7; i++) printf("%.2f  ", a[i]);
+    float a[9] = {3, 4, 100, 12, 10, 5, 9};
+    _heapSort(a, 7);
+    for (i = 0; i < 7; i++)
+        printf("%.2f  ", a[i]);
 }
